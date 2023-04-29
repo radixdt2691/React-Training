30275570c0fcd63462a96064ae656bbff942dfed
radixdt2691 <radixdt.2691@gmail.com>
2023-04-29 18:17:55 +0530

    React Basic Demos

---
+++ personlist-project/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css'
+
+const Person = ({img,name,job,children}) =>{
+    const url= `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+    return(
+        <article className='card-wrapper'>
+            <img src={url} alt="img_person" className='img'/>
+            <h3 className='name'>{name}</h3>
+            <h4 className='job'>{job}</h4>
+        </article>
+    )
+}
+const PersonList = () => {
+    return(
+        <section className='list-wrapper'>
+            <Person img="62" name="Miller" job="Software Developer"/>
+            <Person img="42" name="Stoakes" job="MLOPS"/>
+            <Person img="99" name="Jainish" job="Data Scientist"/>
+        </section>
+    )     
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<PersonList/>)
---
+++ tutorial-1/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+import "./index.css";
+
+function Student(){
+  return(
+    <section className='student-card'>
+        <StudentIDCard />
+        <StudentIDCard />
+        <StudentIDCard />
+        <StudentIDCard />
+    </section>
+  );
+}
+
+const StudentIDCard = () => {
+ const data=[{clgname:'LJ University',clgadd:'Near Sarkhej Cross Road, S.G Highway',clglogo:'https://www.ljku.edu.in/web/image/res.company/1/institute_meta_image'}]
+  return(
+    <article className='card-wrapper'>
+      <h1 className='card-heading'>Student ID-Card</h1>
+      <Studentimage />
+      <Personalinfo/>
+      <Collegeinfo clginfo={data[0]}>
+        <p>This txt is coming from props.children</p>  
+      </Collegeinfo>
+    </article>
+  );
+}
+
+const Studentimage = () => ( <img height="220" className='img-student' src="https://media.istockphoto.com/id/1201229397/photo/happy-young-man-smiling-at-camera.jpg?s=612x612&w=is&k=20&c=mPzI95erBezI1ZlUda9rllQPYyh-kFyF3Tk7BSKHvYg=" alt="student img"></img> )
+
+
+const Personalinfo=()=>{
+  return(
+    <React.Fragment>  
+      <h1>Personal Details</h1>
+      <p className='id'>ID:- 111</p>
+      <p>First Name:- Jainish</p>
+      <p>Last Name:- Gajjar</p>
+      <p>DOB:- 11/09/2000</p>
+    </React.Fragment>
+  )
+}
+
+const Collegeinfo=props=>{
+
+  const{clgname,clgadd,clglogo} = props.clginfo;
+  
+  return(
+    <React.Fragment>
+      <h1>College Details</h1>
+      {props.children}
+      <p>{clgname}</p>
+      <p>{clgadd}</p>
+      <img width="150" src={clglogo} alt='logo_clg'></img>
+   </React.Fragment>
+  )
+}
+
+export default Student;
