18d5160e1e32a03887ac656470c135f2a20f31ce
radixdt2691 <radixdt.2691@gmail.com>
2023-05-18 11:48:19 +0530

    Added propTypes and also defined the default props and isrequired props

--- personlist-project/src/index.js
+++ personlist-project/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import propTypes from "prop-types";
 import './index.css'
 
 const Person = ({img,name,job,children}) =>{
@@ -13,10 +13,18 @@ const Person = ({img,name,job,children}) =>{
         </article>
     )
 }
+Person.propTypes={
+    name: propTypes.string.isRequired,
+    job: propTypes.string
+}
+Person.defaultProps={
+    name: "Jainish", 
+    job: "Bhailang Dev"
+}
 const PersonList = () => {
     return(
         <section className='list-wrapper'>
-            <Person img="62" name="Miller" job="Software Developer"/>
+            <Person img="62" />
             <Person img="42" name="Stoakes" job="MLOPS"/>
             <Person img="99" name="Jainish" job="Data Scientist"/>
         </section>
